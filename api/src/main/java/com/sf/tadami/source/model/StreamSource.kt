f979e8839a47b2708b49f389b5a46513c917c642
Sfoucheur <s.foucheur@gmail.com>
2024-02-22 15:19:48 +0100

    Fix api version

--- api/src/main/java/com/sf/tadami/source/model/StreamSource.kt
+++ api/src/main/java/com/sf/tadami/source/model/StreamSource.kt
@@ -2,14 +2,23 @@ package com.sf.tadami.source.model
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import okhttp3.Headers
 
 @Serializable
 data class StreamSource(
-    val url: String = "",
+    val url : String = "",
     val fullName: String = "",
     val quality: String = "",
     val server: String = "",
-    @Serializable(with = KSerializer::class)
+    @Serializable(with = OkhttpHeadersSerializer::class)
     val headers: Headers? = null
 )
+
+object OkhttpHeadersSerializer : KSerializer<Headers?> {
+    override val descriptor: SerialDescriptor = throw Exception("Stub")
+    override fun serialize(encoder: Encoder, value: Headers?) = throw Exception("Stub")
+    override fun deserialize(decoder: Decoder): Headers? = throw Exception("Stub")
+}
--- api/src/main/java/com/sf/tadami/utils/DataStoreExtensions.kt
+++ api/src/main/java/com/sf/tadami/utils/DataStoreExtensions.kt
@@ -2,132 +2,36 @@ package com.sf.tadami.utils
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.floatPreferencesKey
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.core.stringSetPreferencesKey
 import com.sf.tadami.preferences.model.CustomPreferences
 import com.sf.tadami.preferences.model.CustomPreferencesIdentifier
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.Flow
 
-suspend fun DataStore<Preferences>.getDataStoreValues() =
-    this.data.map { preferences ->
-        preferences
-    }.first()
+suspend fun DataStore<Preferences>.getDataStoreValues() : Preferences = throw Exception("Stub")
 
-suspend fun DataStore<Preferences>.clearAllPreferences() {
-    this.edit { preferences ->
-        preferences.clear()
-    }
-}
-suspend fun DataStore<Preferences>.clearPreferences(names: Set<Preferences.Key<*>>) {
-    this.edit { preferences ->
-        names.forEach {
-            preferences.remove(it)
-        }
-    }
-}
+suspend fun DataStore<Preferences>.clearAllPreferences() : Unit = throw Exception("Stub")
+suspend fun DataStore<Preferences>.clearPreferences(names: Set<Preferences.Key<*>>) : Unit = throw Exception("Stub")
 
-suspend fun <T : CustomPreferencesIdentifier> DataStore<Preferences>.getPreferencesGroup(prefsGroup: CustomPreferences<T>) =
-    this.data.map { preferences ->
-        prefsGroup.transform(preferences)
-    }.first()
+suspend fun <T : CustomPreferencesIdentifier> DataStore<Preferences>.getPreferencesGroup(prefsGroup: CustomPreferences<T>) : T = throw Exception("Stub")
 
-fun <T : CustomPreferencesIdentifier> DataStore<Preferences>.getPreferencesGroupAsFlow(prefsGroup: CustomPreferences<T>) =
-    this.data.map { preferences ->
-        prefsGroup.transform(preferences)
-    }
+fun <T : CustomPreferencesIdentifier> DataStore<Preferences>.getPreferencesGroupAsFlow(prefsGroup: CustomPreferences<T>) : Flow<T> = throw Exception("Stub")
 
 suspend fun <T : CustomPreferencesIdentifier> DataStore<Preferences>.editPreferences(
     newValue: T,
     preferences: CustomPreferences<T>,
     callBack : (newValue : T) -> Unit = {}
-) {
-    this.edit { prefs ->
-        preferences.setPrefs(newValue,prefs)
-        callBack(newValue)
-    }
-}
+) : Unit = throw Exception("Stub")
 
 suspend fun <T> DataStore<Preferences>.editPreference(
     newValue: T,
     preferenceKey: Preferences.Key<T>,
-) {
-    this.edit { prefs ->
-        prefs[preferenceKey] = newValue
-    }
-}
+) : Unit = throw Exception("Stub")
 suspend fun <T> DataStore<Preferences>.createPreference(
     key: Preferences.Key<T>,
     value : T,
-) {
-    this.edit { prefs ->
-        prefs[key] = value
-    }
-}
+) : Unit = throw Exception("Stub")
 
 suspend fun DataStore<Preferences>.replacePreferences(
     filterPredicate: (Map.Entry<Preferences.Key<*>, Any?>) -> Boolean,
     newValue: (Any) -> Any = { it },
     newKey: (Preferences.Key<*>) -> String,
-) {
-    this
-        .getDataStoreValues()
-        .asMap()
-        .filter(filterPredicate)
-        .forEach { (key, value) ->
-            when (value) {
-                is Int -> {
-                    this.createPreference(
-                        intPreferencesKey(newKey(key)),
-                        newValue(value) as Int
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-
-                is Long -> {
-                    this.createPreference(
-                        longPreferencesKey(newKey(key)),
-                        newValue(value) as Long
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-
-                is Float -> {
-                    this.createPreference(
-                        floatPreferencesKey(newKey(key)),
-                        newValue(value) as Float
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-
-                is String -> {
-                    this.createPreference(
-                        stringPreferencesKey(newKey(key)),
-                        newValue(value) as String
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-
-                is Boolean -> {
-                    this.createPreference(
-                        booleanPreferencesKey(newKey(key)),
-                        newValue(value) as Boolean
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-
-                is Set<*> -> (value as? Set<String>)?.let {
-                    this.createPreference(
-                        stringSetPreferencesKey(newKey(key)),
-                        newValue(value) as Set<String>
-                    )
-                    this.clearPreferences(setOf(key))
-                }
-            }
-        }
-}
+) : Unit = throw Exception("Stub")
