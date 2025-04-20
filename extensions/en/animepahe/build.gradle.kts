8b65149738b523cd5eaaf83ced83543bb54319de
Sfoucheur <s.foucheur@gmail.com>
2025-04-20 19:55:06 +0200

    chore: bump animepahe

--- extensions/en/animepahe/build.gradle.kts
+++ extensions/en/animepahe/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 buildscript {
     extra.apply {
-        set("extVersionCode",2)
+        set("extVersionCode",3)
         set("extName","AnimePahe")
         set("pkgNameSuffix","en.animepahe")
         set("extClass",".AnimePahe")
