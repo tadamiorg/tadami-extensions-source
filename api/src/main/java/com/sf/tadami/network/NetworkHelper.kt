9f364d2468d5026cbbcef74648cd0bba9d099657
Sfoucheur <s.foucheur@gmail.com>
2025-08-15 10:58:04 +0200

    fix(api): added nonCloudFlareClient

--- api/src/main/java/com/sf/tadami/network/NetworkHelper.kt
+++ api/src/main/java/com/sf/tadami/network/NetworkHelper.kt
@@ -7,6 +7,8 @@ class NetworkHelper(private val context : Context) {
 
     val client: OkHttpClient = throw Exception("Stub!")
 
+    val nonCloudflareClient: OkHttpClient = throw Exception("Stub!")
+
     val cloudflareClient: OkHttpClient = throw Exception("Stub!")
     companion object{
         const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63"
